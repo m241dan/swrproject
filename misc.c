c5a5f952cb38b236d80b82e43699ae649307fdfe
Davenge <korisd@gmail.com>
2013-05-06 21:37:15 -0400

    added the framework for extended bv, need to convert affs now

--- misc.c
+++ misc.c
@@ -21,6 +21,7 @@
 * getting big.								   *
 ****************************************************************************/
 
+#include <stdarg.h>
 #include <sys/types.h>
 #include <stdio.h>
 #include <string.h>
@@ -2943,3 +2944,178 @@ void do_bank( CHAR_DATA * ch, const char *argument )
       return;
    }
 }
+/*
+ * Extended Bitvector Routines                                  -Thoric
+ */
+
+/* check to see if the extended bitvector is completely empty */
+bool ext_is_empty( EXT_BV * bits )
+{
+   int x;
+
+   for( x = 0; x < XBI; x++ )
+      if( bits->bits[x] != 0 )
+         return FALSE;
+
+   return TRUE;
+}
+
+void ext_clear_bits( EXT_BV * bits )
+{
+   int x;
+
+   for( x = 0; x < XBI; x++ )
+      bits->bits[x] = 0;
+}
+
+/* for use by xHAS_BITS() -- works like IS_SET() */
+int ext_has_bits( EXT_BV * var, EXT_BV * bits )
+{
+   int x, bit;
+
+   for( x = 0; x < XBI; x++ )
+      if( ( bit = ( var->bits[x] & bits->bits[x] ) ) != 0 )
+         return bit;
+
+   return 0;
+}
+
+/* for use by xSAME_BITS() -- works like == */
+bool ext_same_bits( EXT_BV * var, EXT_BV * bits )
+{
+   int x;
+
+   for( x = 0; x < XBI; x++ )
+      if( var->bits[x] != bits->bits[x] )
+         return FALSE;
+
+   return TRUE;
+}
+
+/* for use by xSET_BITS() -- works like SET_BIT() */
+void ext_set_bits( EXT_BV * var, EXT_BV * bits )
+{
+   int x;
+
+   for( x = 0; x < XBI; x++ )
+      var->bits[x] |= bits->bits[x];
+}
+
+/* for use by xREMOVE_BITS() -- works like REMOVE_BIT() */
+void ext_remove_bits( EXT_BV * var, EXT_BV * bits )
+{
+   int x;
+
+   for( x = 0; x < XBI; x++ )
+      var->bits[x] &= ~( bits->bits[x] );
+}
+
+/* for use by xTOGGLE_BITS() -- works like TOGGLE_BIT() */
+void ext_toggle_bits( EXT_BV * var, EXT_BV * bits )
+{
+   int x;
+
+   for( x = 0; x < XBI; x++ )
+      var->bits[x] ^= bits->bits[x];
+}
+/*
+ * Read an extended bitvector from a file.                      -Thoric
+ */
+EXT_BV fread_bitvector( FILE * fp )
+{
+   EXT_BV ret;
+   int c, x = 0;
+   int num = 0;
+
+   memset( &ret, '\0', sizeof( ret ) );
+   for( ;; )
+   {
+      num = fread_number( fp );
+      if( x < XBI )
+         ret.bits[x] = num;
+      ++x;
+      if( ( c = getc( fp ) ) != '&' )
+      {
+         ungetc( c, fp );
+         break;
+      }
+   }
+
+   return ret;
+}
+
+/* return a string for writing a bitvector to a file */
+char *print_bitvector( EXT_BV * bits )
+{
+   static char buf[XBI * 12];
+   char *p = buf;
+   int x, cnt = 0;
+
+   for( cnt = XBI - 1; cnt > 0; cnt-- )
+      if( bits->bits[cnt] )
+         break;
+   for( x = 0; x <= cnt; x++ )
+   {
+      snprintf( p, ( XBI * 12 ) - ( p - buf ), "%d", bits->bits[x] );
+      p += strlen( p );
+      if( x < cnt )
+         *p++ = '&';
+   }
+   *p = '\0';
+
+   return buf;
+}
+
+/*
+ * Write an extended bitvector to a file                        -Thoric
+ */
+void fwrite_bitvector( EXT_BV * bits, FILE * fp )
+{
+   fputs( print_bitvector( bits ), fp );
+}
+
+EXT_BV meb( int bit )
+{
+   EXT_BV bits;
+
+   xCLEAR_BITS( bits );
+   if( bit >= 0 )
+      xSET_BIT( bits, bit );
+
+   return bits;
+}
+
+EXT_BV multimeb( int bit, ... )
+{
+   EXT_BV bits;
+   va_list param;
+   int b;
+
+   xCLEAR_BITS( bits );
+   if( bit < 0 )
+      return bits;
+
+   xSET_BIT( bits, bit );
+
+   va_start( param, bit );
+
+   while( ( b = va_arg( param, int ) ) != -1 )
+        xSET_BIT( bits, b );
+
+   va_end( param );
+
+   return bits;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- mud.h
+++ mud.h
@@ -116,7 +116,7 @@ typedef struct cmd_type CMDTYPE;
 typedef struct killed_data KILLED_DATA;
 typedef struct wizent WIZENT;
 typedef struct specfun_list SPEC_LIST;
-
+typedef struct extended_bitvector EXT_BV;
 /*
 * Function types.
 */
@@ -297,6 +297,26 @@ typedef enum
 #define WT_IMM    2
 #define WT_AVATAR 1
 #define WT_NEWBIE 3
+
+/*
+ * Defines for extended bitvectors
+ */
+#ifndef INTBITS
+#define INTBITS 32
+#endif
+#define XBM             31 /* extended bitmask   ( INTBITS - 1 )  */
+#define RSV             5  /* right-shift value  ( sqrt(XBM+1) )  */
+#define XBI             4  /* integers in an extended bitvector   */
+#define MAX_BITS        XBI * INTBITS
+/*
+ * Structure for extended bitvectors -- Thoric
+ */
+struct extended_bitvector
+{
+   unsigned int bits[XBI];
+};
+
+
 
 #include "dns.h"
 #include "color.h"
@@ -2774,6 +2794,43 @@ int urange( int mincheck, int check, int maxcheck );
 #define SET_BIT(var, bit)	((var) |= (bit))
 #define REMOVE_BIT(var, bit)	((var) &= ~(bit))
 #define TOGGLE_BIT(var, bit)	((var) ^= (bit))
+
+/*
+ * Macros for accessing virtually unlimited bitvectors.         -Thoric
+ *
+ * Note that these macros use the bit number rather than the bit value
+ * itself -- which means that you can only access _one_ bit at a time
+ *
+ * This code uses an array of integers
+ */
+
+/*
+ * The functions for these prototypes can be found in misc.c
+ * They are up here because they are used by the macros below
+ */
+bool ext_is_empty args( ( EXT_BV * bits ) );
+void ext_clear_bits args( ( EXT_BV * bits ) );
+int ext_has_bits args( ( EXT_BV * var, EXT_BV * bits ) );
+bool ext_same_bits args( ( EXT_BV * var, EXT_BV * bits ) );
+void ext_set_bits args( ( EXT_BV * var, EXT_BV * bits ) );
+void ext_remove_bits args( ( EXT_BV * var, EXT_BV * bits ) );
+void ext_toggle_bits args( ( EXT_BV * var, EXT_BV * bits ) );
+
+/*
+ * Here are the extended bitvector macros:
+ */
+#define xIS_SET(var, bit)       ((var).bits[(bit) >> RSV] & 1 << ((bit) & XBM))
+#define xSET_BIT(var, bit)      ((var).bits[(bit) >> RSV] |= 1 << ((bit) & XBM))
+#define xSET_BITS(var, bit)     (ext_set_bits(&(var), &(bit)))
+#define xREMOVE_BIT(var, bit)   ((var).bits[(bit) >> RSV] &= ~(1 << ((bit) & XBM)))
+#define xREMOVE_BITS(var, bit)  (ext_remove_bits(&(var), &(bit)))
+#define xTOGGLE_BIT(var, bit)   ((var).bits[(bit) >> RSV] ^= 1 << ((bit) & XBM))
+#define xTOGGLE_BITS(var, bit)  (ext_toggle_bits(&(var), &(bit)))
+#define xCLEAR_BITS(var)        (ext_clear_bits(&(var)))
+#define xIS_EMPTY(var)          (ext_is_empty(&(var)))
+#define xHAS_BITS(var, bit)     (ext_has_bits(&(var), &(bit)))
+#define xSAME_BITS(var, bit)    (ext_same_bits(&(var), &(bit)))
+
 
 /*
 * Memory allocation macros.
