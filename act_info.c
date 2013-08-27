ed1fcbd22be91443b72cd92db2b619f746e0aa5c
Davenge Coders <korisd@gmail.com>
2013-08-26 17:21:19 -0700

    quick formatting change to notorious

--- act_info.c
+++ act_info.c
@@ -518,7 +518,7 @@ void show_char_to_char_0( CHAR_DATA * victim, CHAR_DATA * ch )
    if( IS_NPC( victim ) && victim->pIndexData->first_available_quest )
       strcat( buf, "&z(&RQuest&z)&w " );
    if( IS_NPC( victim ) && IS_SET( victim->act, ACT_NOTORIOUS ) )
-      strcat( buf, "&z(&PNotorious&z)&w" );
+      strcat( buf, "&z(&PNotorious&z)&w " );
 
 
    set_char_color( AT_PERSON, ch );
